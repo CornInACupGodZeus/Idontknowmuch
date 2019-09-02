65e75c44ccecbfd162e60ff3bbf6795b5c126bf3
CornInACupGodZeus <54526958+CornInACupGodZeus@users.noreply.github.com>
2019-09-02 17:00:59 +0300

    pyler eklendi

---
+++ AddProduct.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+i=input("Enter first number:")
+i= int(i)
+e=input("Enter second number:")
+e= int(e)
+plus = i + e
+product = i * e
+if product>1000:
+    print ("result is",plus)
+else:
+    print ("result is",product)
---
+++ CmdNumpadCharacters.py
@@ -0,0 +1,5 @@
+x=0
+while x<255:
+
+    print(str(x)," == ",chr(x))
+    x+=1
